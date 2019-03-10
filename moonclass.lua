6aca8c95d471884ff3e99cee3e425531ef83a28b
Grump <megagrump@protonmail.com>
2019-03-10 13:12:41 +0100

    refactoring

--- moonclass.lua
+++ moonclass.lua
@@ -1,59 +1,51 @@
---[[
-moonclass - moonscript compatible class implementation for Lua
-Copyright (c) 2019 megagrump@pm.me
-License: MIT
-See LICENSE for details
-]]
+--[[-----------------------------------------------------------------
+moonclass - moonscript compatible class implementation
+
+Copyright 2019 megagrump@pm.me
+License: MIT. See LICENSE for details
+]]-------------------------------------------------------------------
 local moonclass = {}
 
 local function setupClass(name, base, parent)
-	return {
+	local mt = {
+		__call = function(cls, ...)
+			local self = setmetatable({}, base)
+			cls.__init(self, ...)
+			return self
+		end,
+	}
+
+	return setmetatable({
 		__name = name,
 		__base = base,
 		__parent = parent,
 		__init = function(...)
 			return base.initialize(...)
 		end,
-	}
+	}, mt), mt
 end
 
 local function defineClass(name, base)
-	base.__index = base
-
-	local class = setmetatable(setupClass(name, base), {
-		__index = base,
-		__call = function(cls, ...)
-			local self = setmetatable({}, base)
-			cls.__init(self, ...)
-			return self
-		end,
-	})
-
-	base.__class = class
+	local class, mt = setupClass(name, base)
+	mt.__index, base.__class, base,__index = base, class, base
 	return class
 end
 
 function moonclass.extend(name, parent, base)
-	base.__index = base
 	setmetatable(base, parent.__base)
 
-	local class = setmetatable(setupClass(name, base, parent), {
-		__index = function(cls, name)
-			local val = rawget(base, name)
-			if val == nil then return parent[name] end
-			return val
-		end,
-		__call = function(cls, ...)
-			local self = setmetatable({}, base)
-			cls.__init(self, ...)
-			return self
-		end,
-	})
+	local class, mt = setupClass(name, base, parent)
+	mt.__index = function(cls, name)
+		local val = rawget(base, name)
+		if val == nil then return parent[name] end
+		return val
+	end
 
-	base.__class = class
+	base.__class, base.__index = class, base
 	if parent.__inherited then
 		parent.__inherited(parent, class)
 	end
+
 	return class
 
 end
