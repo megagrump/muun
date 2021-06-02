10b20a30f1f6a1cd38cf7f69b7955b70f36496a1
megagrump <megagrump@protonmail.com>
2021-06-02 13:35:25 +0200

    new super semantics

--- muun.lua
+++ muun.lua
@@ -1,7 +1,7 @@
 local _ = [[-----------------------------------------------------------------
 -- muun - moonscript compatible class implementation
 
--- Copyright 2019 megagrump@pm.me
+-- Copyright 2019, 2021 megagrump@pm.me
 -- License: MIT. See LICENSE for details
 -----------------------------------------------------------------]]
 local setup
@@ -12,58 +12,56 @@ setup = function(__name, __parent, __base)
       self.__init(obj, ...)
       return obj
     end,
+    __index = __base,
     __newindex = function(self, key, value)
       __base[key] = value
     end
   }
   __base.new = __base.new or function(self) end
   return setmetatable({
-    __name = __name,
-    __base = __base,
-    __parent = __parent,
     __init = function(...)
       return __base.new(...)
-    end
+    end,
+    __name = __name,
+    __base = __base,
+    __parent = __parent
   }, mt), mt
+end
+local super
+super = function(parent)
+  return setmetatable({ }, {
+    __call = function(self, this, ...)
+      return parent.__init(this, ...)
+    end,
+    __index = parent
+  })
 end
 local extend
 extend = function(name, parent, base)
   setmetatable(base, parent.__base)
   local cls, mt = setup(name, parent, base)
-  base.__class, base.__index = cls, base
-  mt.__index = function(self, key)
-    local val = rawget(base, key)
-    if val ~= nil then
-      return val
-    else
-      return parent[key]
-    end
+  do
+    base.__class, base.__index = cls, base
+    base.__super = super(parent)
   end
   if parent.__inherited then
     parent.__inherited(parent, cls)
   end
   return cls
 end
-local muun = {
-  super = function(self, ...)
-    return self.__class.__parent.__init(self, ...)
+return function(name, parentOrBase, base)
+  if type(name) ~= 'string' then
+    error("Invalid class name")
   end
-}
-return setmetatable(muun, {
-  __call = function(self, name, parentOrBase, base)
-    if type(name) ~= 'string' then
-      error("Invalid class name")
-    end
-    local parent
-    if type(parentOrBase) == 'table' and parentOrBase.__class then
-      parent = parentOrBase
-    end
-    base = not parent and parentOrBase or base or { }
-    if parent then
-      return extend(name, parent, base)
-    end
-    local cls, mt = setup(name, nil, base)
-    mt.__index, base.__class, base.__index = base, cls, base
-    return cls
+  local parent
+  if type(parentOrBase) == 'table' and parentOrBase.__class then
+    parent = parentOrBase
   end
-})
+  base = not parent and parentOrBase or base or { }
+  if parent then
+    return extend(name, parent, base)
+  end
+  local cls, mt = setup(name, nil, base)
+  mt.__index, base.__class, base.__index = base, cls, base
+  return cls
+end
--- muun.moon
+++ muun.moon
@@ -1,7 +1,7 @@
 [[-----------------------------------------------------------------
 -- muun - moonscript compatible class implementation
 
--- Copyright 2019 megagrump@pm.me
+-- Copyright 2019, 2021 megagrump@pm.me
 -- License: MIT. See LICENSE for details
 -----------------------------------------------------------------]]
 
@@ -11,38 +11,42 @@ setup = (__name, __parent, __base) ->
 			obj = setmetatable({}, __base)
 			@.__init(obj, ...)
 			obj
+		__index: __base
 		__newindex: (key, value) => __base[key] = value
+
 	__base.new or= =>
 
 	setmetatable({
-		:__name, :__base, :__parent, __init: (...) -> __base.new(...)
+		__init: (...) -> __base.new(...)
+		:__name
+		:__base
+		:__parent
 	}, mt), mt
+
+super = (parent) ->
+	setmetatable({}, {
+		__call: (this, ...) => parent.__init(this, ...)
+		__index: parent
+	})
 
 extend = (name, parent, base) ->
 	setmetatable(base, parent.__base)
 
 	cls, mt = setup(name, parent, base)
-	base.__class, base.__index = cls, base
-
-	mt.__index = (key) =>
-		val = rawget(base, key)
-		val if val ~= nil else parent[key]
+	with base
+		.__class, .__index = cls, base
+		.__super = super(parent)
 
 	parent.__inherited(parent, cls) if parent.__inherited
 	cls
 
-muun =
-	super: (...) => @.__class.__parent.__init(@, ...)
+(name, parentOrBase, base) ->
+	error("Invalid class name") if type(name) ~= 'string'
 
-setmetatable(muun, {
-	__call: (name, parentOrBase, base) =>
-		error("Invalid class name") if type(name) ~= 'string'
+	parent = parentOrBase if type(parentOrBase) == 'table' and parentOrBase.__class
+	base = not parent and parentOrBase or base or {}
+	return extend(name, parent, base) if parent
 
-		parent = parentOrBase if type(parentOrBase) == 'table' and parentOrBase.__class
-		base = not parent and parentOrBase or base or {}
-		return extend(name, parent, base) if parent
-
-		cls, mt = setup(name, nil, base)
-		mt.__index, base.__class, base.__index = base, cls, base
-		cls
-})
+	cls, mt = setup(name, nil, base)
+	mt.__index, base.__class, base.__index = base, cls, base
+	cls
